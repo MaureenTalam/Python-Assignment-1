8e62d7f6a09bd49867a4307cfb1dcd4f81044dff
MaureenTalam <mojeptalaam@gmail.com>
2024-09-10 15:48:06 +0300

    answers to python assignment

--- user_input.py
+++ user_input.py
@@ -1,3 +1,5 @@
+##Write a program that asks the user for their name, age, and location and then prints out a personalized message.
+
 name = input("Enter your name:")
 age = input("Enter your age:")
 location = input("Enter your location:")
